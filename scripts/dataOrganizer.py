1962b7e97208713cfb453d668f0efcfebc65a7e6
Linß, Arthur <arthur.linss@dlr.de>
2023-09-04 16:03:17 +0200

    minor

--- scripts/dataOrganizer.py
+++ scripts/dataOrganizer.py
@@ -2,7 +2,8 @@ import os
 import tarfile
 from six.moves import urllib
 
-def fetchData(url: str=None, path: str=None):
+
+def fetchData(url: str = None, path: str = None):
     """
     create local directory for the output given path argument
     download data given as tar ball given a url argument
@@ -17,11 +18,13 @@ def fetchData(url: str=None, path: str=None):
     tgz.close()
     print("fetching data succesful")
 
+
 import pandas as pd
 import pyspark.pandas as ps
 import gzip
 
-def loadDataAsDF(pathToFile: str=None, spark=True):
+
+def loadDataAsDF(pathToFile: str = None, spark=True):
     """
     load csv file as pandas dataframe given a local path and a filename
     bool spark enables to return a pyspark dataframe instead of a pandas dataframe
@@ -31,7 +34,7 @@ def loadDataAsDF(pathToFile: str=None, spark=True):
     try:
         df = pd.read_csv(pathToFile)
     except:
-        with gzip.open(pathToFile, 'rt', encoding='utf-8') as f:
+        with gzip.open(pathToFile, "rt", encoding="utf-8") as f:
             df = pd.read_csv(f)
     if spark:
         sparkDF = ps.from_pandas(df)
@@ -39,3 +42,40 @@ def loadDataAsDF(pathToFile: str=None, spark=True):
     return df
 
 
+def inspectDFCol(df: pd.DataFrame = None, coloumn: str = ""):
+    """
+    inspect a dataframe column
+    different values and count
+    """
+    val_count = df[coloumn].value_counts()
+    print("\nvalue count: \n", val_count)
+
+
+import matplotlib.pyplot as plt
+
+
+def quickHistogram(df: pd.DataFrame = None):
+    """
+    create a quick and dirty histogram of all dataframe columns
+    """
+    df.hist(bins=50, figsize=(20, 15))
+    plt.show()
+
+
+import numpy as np
+
+
+def splitTrainTest(df: pd.DataFrame = None, ratio=0.2):
+    """
+    manually split a dataframe into a train and test set
+    returns train and test df
+    ratio is the relative of test to train set
+    """
+    np.random.seed(42)
+    shuffled = np.random.permutation(len(df))
+    testSetSize = int(len(df) * ratio)
+    testIndices = shuffled[:testSetSize]
+    trainIndices = shuffled[testSetSize:]
+    retValTrain = df.iloc[trainIndices]
+    retValTest = df.iloc[testIndices]
+    return retValTrain, retValTest
--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,43 @@
-from dataOrganizer import fetchData, loadDataAsDF
+from dataOrganizer import (
+    fetchData,
+    loadDataAsDF,
+    inspectDFCol,
+    quickHistogram,
+    splitTrainTest,
+)
 
-def readingData():
-    verbose = True
+
+def readingData(verbose=True, plotting=True):
     # define path for download
     ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
     PATH = "datasets/housing"
     FILE = "/housing.tgz"
     URL = ROOT + PATH + FILE
     fetchData(url=URL, path=PATH)
-    df = loadDataAsDF(pathToFile = PATH + FILE, spark=False)
+    df = loadDataAsDF(pathToFile=PATH + FILE, spark=False)
     if verbose:
         print(df.head(10))
         df.info()
         print("Type: %s " % type(df))
 
+        inspectDFCol(df, "ocean_proximity")
+
+        print(df.describe())
+
+    if plotting:
+        quickHistogram(df)
+
+    return df
+
 
 def main():
-    readingData()
+    df = readingData(verbose=False, plotting=False)
+    df_train, df_test = splitTrainTest(df, 0.2)
+    # testing the train test split
+    print("train set: ", len(df_train))
+    print("test set: ", len(df_test))
+    print("ratio test/train: %.2f " % float(len(df_test) / len(df_train)))
+
 
 if __name__ == "__main__":
     main()
