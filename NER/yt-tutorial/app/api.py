c3ecd2aaefc1e3668a50a80481a56afd5bdc4939
Linß, Arthur <arthur.linss@dlr.de>
2024-01-26 14:05:12 +0100

    small update

--- NER/yt-tutorial/app/api.py
+++ NER/yt-tutorial/app/api.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, jsonify
 import spacy
 from flask_cors import CORS
-
+from sim_backend import get_similarity
+import json
 
 # checkout: python -m spacy download en_core_web_sm
 
@@ -47,6 +48,25 @@ def render_displacy(doc, style):
 def dropdown_values():
     display_styles = ['ent', 'dep']
     return jsonify({'dropdownValues': display_styles})
+
+
+@app.route('/sum_text', methods=['POST'])
+def sum_text():
+    if request.method == 'POST':
+        data = request.get_json()
+        text1 = data.get('text1', '')
+        text2 = data.get('text2', '')
+
+        # Perform the sum operation (you can replace this with your desired logic)
+        common_words, common_topics, common_topics_names = get_similarity(text1, text2)
+
+        common_words = '%s' % json.dumps(list(common_words))
+        print("common_words: ", common_words)
+        print("topic names: ", common_topics_names)
+        return jsonify({'result': common_words}), 200
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
